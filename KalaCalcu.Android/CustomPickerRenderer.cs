37ce96646a9964e8e0d3181d7b6fccdae2264f27
Ralmoguez <67216820+Ralmoguez@users.noreply.github.com>
2023-11-05 09:48:59 +0800

    Initial commit

---
+++ KalaCalcu.Android/CustomPickerRenderer.cs
@@ -0,0 +1,34 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.OS;
+using KalaCalcu.Droid;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Color = Android.Graphics.Color;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(CustomPickerRenderer))]
+namespace KalaCalcu.Droid
+{
+    public class CustomPickerRenderer : PickerRenderer
+    {
+        private Context context;
+        public CustomPickerRenderer(Context context) : base(context)
+        {
+            this.context = context;
+        }
+
+        
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null || e.NewElement == null) return;
+            //for example ,change the line to red:
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+                Control.BackgroundTintList = ColorStateList.ValueOf(Color.Transparent);
+            else
+                Control.Background.SetColorFilter(Color.Red, PorterDuff.Mode.SrcAtop);
+        }
+    }
+}
